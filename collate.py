e1adc4aa5614f232575b9d5fb974386d343de6b1
nickphoon <potatonicholas@gmail.com>
2023-08-24 14:34:45 +0800

    minor bugfix

--- collate.py
+++ collate.py
@@ -15,12 +15,17 @@ def scrape_local_folder(folder_path, json_data):
 
 def scrape(folder_path):
     folder_name = folder_path.split("/")
+    
+    if "github" in folder_name[-1]:
+        final_folder_name = "reviewed"
+    else:
+        final_folder_name = folder_name[-1]
     script_folder = os.path.dirname(os.path.abspath(__file__))
     full_folder_path = os.path.join(script_folder, folder_path)
     
     for entry in os.listdir(full_folder_path):
         all_json_data = []
-        json_filename = folder_name+'advisory_data'+entry+'.json' 
+        json_filename = final_folder_name+'advisory_data'+entry+'.json' 
         new_path = os.path.join(full_folder_path,entry)
         
         scrape_local_folder(new_path, all_json_data)
@@ -28,10 +33,10 @@ def scrape(folder_path):
             json.dump(all_json_data, jsonfile, indent=4)
 
     single_json_data = []
-    single_json_filename = folder_name + 'advisory_data.json'
+    single_json_filename = final_folder_name + 'advisory_data.json'
      
     for file in os.listdir(script_folder):
-        if file.startswith(folder_name+ "advisory_data2"):
+        if file.startswith(final_folder_name+ "advisory_data2"):
             file_path = os.path.join(script_folder, file)
             with open(file_path, "r") as json_file:
                 data = json.load(json_file)
